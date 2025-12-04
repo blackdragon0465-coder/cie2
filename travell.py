4c688524ed3648c38cd7b2737a6480dc2a4a1ee2
blackdragon0465-coder <blackdragon0465@gmail.com>
2025-12-04 17:00:43 +0530

    Fix condition check and improve output formatting

--- travell.py
+++ travell.py
@@ -1,6 +1,8 @@
 import sys
-if (sys.argv) == 4:
+
+if len(sys.argv) == 4:
     print("User input given")
+
     script_name = sys.argv[0]
     distancetravelled = float(sys.argv[1])
     fuelused = float(sys.argv[2])
@@ -8,11 +10,12 @@ if (sys.argv) == 4:
 
 else:
     print("User input not given")
+
     script_name = sys.argv[0]
     distancetravelled = 3.0
     fuelused = 2.0
     mileage = 40.0
 
-    print("Didtance Travelled km:", distancetravelled)
-    print("Fuel Used liters:", fuelused)
-    print("Mileage km/liter:", mileage)
+print("Distance Travelled km:", distancetravelled)
+print("Fuel Used liters:", fuelused)
+print("Mileage km/liter:", mileage)
